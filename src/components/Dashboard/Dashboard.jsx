46ccffb8963d5994e91373981684f0f82fdd1f2c
Adam Bergstrom <abx1138@gmail.com>
2023-03-07 16:15:00 -0600

    updated dashboard with tables and mock data

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -15,40 +15,49 @@ function Dashboard() {
 
   return (
     <div className="container">
-      <h2>{heading}</h2>
+      {/* <h2>{heading}</h2> */}
+      <h1>nextLEVEL - Operator Dashboard</h1>
 
       <div className="grid">
         <div className="grid-col grid-col_8">
           <table>
-            
+            <tr>
+              <th>Client Name</th>
+              <th>Process Step</th>
+              <th>Edit</th>
+            </tr>
+            <tr>
+              <td>Test Client</td>
+              <td>1</td>
+              <td><button>Edit</button></td>
+            </tr>
           </table>
-          <p>
-            Dashboard - In Progress needs table
-          </p>
-          <p>
-          Client Name Process Step Edit
-          </p>
         </div>
-        {/* <form className="form-panel"> */}
           <div className="grid-col grid-col_4">
-          <button>Create New Client</button>
-
-            {/* <RegisterForm /> */}
-            {/* <center>
-              <h4>Already a Member?</h4>
-              <button className="btn btn_sizeSm" onClick={onLogin}>
-                Login
-              </button>
-            </center> */}
+            <button>Create New Client</button>
           </div>
-        {/* </form> */}
+
         <div className="grid-col grid-col_10">
-          <p>
-            Dashboard - Current Clients needs table
-          </p>
-          <p>
-            Client Name, Engagement Start, Current Phase, Level, Edit Button, See Details Button, Save Assessment Button
-          </p>
+          <table>
+            <tr>
+              <th>Client Name</th>
+              <th>Engagement Start</th>
+              <th>Current Phase</th>
+              <th>Level</th>
+              <th>Edit</th>
+              <th>See Detail</th>
+              <th>See Assessment</th>
+            </tr>
+            <tr>
+              <td>Test Client</td>
+              <td>02/28/2023</td>
+              <td>Phase 2</td>
+              <td>3</td>
+              <td><button>Edit</button></td>
+              <td><button>See Details</button></td>
+              <td><button>Save Assessment</button></td>
+            </tr>
+          </table>
         </div>
       </div>
     </div>
