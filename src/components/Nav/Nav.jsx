65c20b39dd2e1dbc62dd2c401e605718244073fd
ibrah100 <37915317+ibrah100@users.noreply.github.com>
2023-03-08 11:52:09 -0600

    finished layout of sidebar, still needs clickability to go to different pages

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -8,39 +8,70 @@ function Nav() {
   const user = useSelector((store) => store.user);
 
   return (
-    <div className="nav">
-      <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
-      </Link>
-      <div>
-        {/* If no user is logged in, show these links */}
-        {!user.id && (
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
-        )}
-
-        {/* If a user is logged in, show these links */}
-        {user.id && (
-          <>
-            <Link className="navLink" to="/user">
-              Home
-            </Link>
-
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
-
-            <LogOutButton className="navLink" />
-          </>
-        )}
-
-        <Link className="navLink" to="/about">
-          About
-        </Link>
-      </div>
+    <div class="container-fluid">
+    <div class="row flex-nowrap">
+        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-secondary">
+            <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                    <span class="fs-5 d-none d-sm-inline">nextLevel</span>
+                </a>
+                <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link align-middle px-0 text-dark">
+                            <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Dashboard</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle text-dark">
+                            <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Assessments</span> </a>
+                        <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
+                            <li class="w-100">
+                                <a href="#" class="nav-link px-0 text-dark"> <span class="d-none d-sm-inline">- Organizational Design</span></a>
+                            </li>
+                            <li>
+                                <a href="#" class="nav-link px-0 text-dark"> <span class="d-none d-sm-inline">- Employee Engagement</span></a>
+                            </li>
+                            <li>
+                                <a href="#" class="nav-link px-0 text-dark"> <span class="d-none d-sm-inline">- Training & Development</span></a>
+                            </li>
+                            <li>
+                                <a href="#" class="nav-link px-0 text-dark"> <span class="d-none d-sm-inline">- Benefits & Compensation</span></a>
+                            </li>
+                            <li>
+                                <a href="#" class="nav-link px-0 text-dark"> <span class="d-none d-sm-inline">- Recruiting & Staffing</span></a>
+                            </li>
+                            <li>
+                                <a href="#" class="nav-link px-0 text-dark"> <span class="d-none d-sm-inline">- HRIS, Payroll & Compliance</span></a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-0 align-middle text-dark">
+                            <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">All Clients</span></a>
+                    </li>
+                </ul>
+                <hr></hr>
+                {/* <div class="dropdown pb-4">
+                    <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                        <span class="d-none d-sm-inline mx-1">Malik Ibrahim</span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
+                        <li><a class="dropdown-item" href="#">New project...</a></li>
+                        <li><a class="dropdown-item" href="#">Settings</a></li>
+                        <li><a class="dropdown-item" href="#">Profile</a></li>
+                        <li>
+                            <hr class="dropdown-divider"></hr>
+                        </li>
+                        <li><a class="dropdown-item" href="#">Sign out</a></li>
+                    </ul>
+                </div> */}
+            </div>
+        </div>
+        <div class="col py-3">
+            Content area...
+        </div>
     </div>
+</div>
   );
 }
 
