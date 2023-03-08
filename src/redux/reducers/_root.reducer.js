e9e4f7466f5f6ce829e650d982c0ab516d54201d
Lauren <lauren.a.heavey@gmail.com>
2023-03-08 15:08:13 -0600

    Add sagas for assessment structure, adjust comments on reducer.

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -12,7 +12,7 @@ import structure from './assessment.structure.reducer';
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
-  structure, // contains buckets, functions by bucket, subfunctions by function
+  structure, // contains buckets, functions by bucket, subfunctions by function (as objects)
 });
 
 export default rootReducer;
