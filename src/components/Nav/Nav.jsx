459fc0930408ece2b7c6aa5e18d209d1f52c76c5
ibrah100 <37915317+ibrah100@users.noreply.github.com>
2023-03-08 12:39:48 -0600

    added toggle button for sidebar, links to dashboard and all clients

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -10,21 +10,21 @@ function Nav() {
   return (
     <div class="container-fluid">
     <div class="row flex-nowrap">
-        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-secondary">
+        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-secondary" id="sidebar">
             <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
                 <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
                     <span class="fs-5 d-none d-sm-inline">nextLevel</span>
                 </a>
                 <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                     <li class="nav-item">
-                        <a href="#" class="nav-link align-middle px-0 text-dark">
+                        <a href="/dashboard" class="nav-link align-middle px-0 text-dark">
                             <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Dashboard</span>
                         </a>
                     </li>
                     <li>
                         <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle text-dark">
                             <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Assessments</span> </a>
-                        <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
+                        <ul class="collapse nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
                             <li class="w-100">
                                 <a href="#" class="nav-link px-0 text-dark"> <span class="d-none d-sm-inline">- Organizational Design</span></a>
                             </li>
@@ -46,7 +46,7 @@ function Nav() {
                         </ul>
                     </li>
                     <li>
-                        <a href="#" class="nav-link px-0 align-middle text-dark">
+                        <a href="/all-clients" class="nav-link px-0 align-middle text-dark">
                             <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">All Clients</span></a>
                     </li>
                 </ul>
@@ -68,6 +68,7 @@ function Nav() {
             </div>
         </div>
         <div class="col py-3">
+          <button data-bs-toggle="collapse" data-bs-target="#sidebar">Toggle Menu</button>
             Content area...
         </div>
     </div>
