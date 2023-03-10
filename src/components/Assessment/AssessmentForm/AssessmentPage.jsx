2c3f0ba077f0fb4cf860a2d0ddc2a3763fa7e0c4
Lauren <lauren.a.heavey@gmail.com>
2023-03-10 08:59:03 -0600

    Additional page styling, add progress bar.

--- src/components/Assessment/AssessmentForm/AssessmentPage.jsx
+++ src/components/Assessment/AssessmentForm/AssessmentPage.jsx
@@ -11,56 +11,65 @@ function AssessmentPage ({functionsArray}) {
   const subfunctionsArray = structure.subfunctionsReducer;
 
   console.log('Params: ', Number(params.function_id))
-  console.log('functionsArray: ', functionsArray)
+  console.log('functionsArray: ', functionsArray.length)
   console.log('subfunctionsArray: ', subfunctionsArray)
+  // console.log('functionsArray: ', functionsArray[9].function_index)
 
   return (
     <>
     {functionsArray.map((functionObject) => {
+      console.log('Function Object: ', functionObject);
       if (Number(params.function_id) === functionObject.id) {
         return (
           <div key={functionObject.id}>
-            <h4>{functionObject.name}</h4>
+            <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow={functionObject.function_index} aria-valuemin="0" aria-valuemax={functionsArray.length}>
+              <div className="progress-bar w-75"></div>
+            </div>
+            <h3>{functionObject.name}</h3>
+
             {/* <AssessmentSection subfunctionsArray={subfunctionsArray}/> */}
             {subfunctionsArray.map((subfunction) => {
               return (
-                <form key={subfunction.id} className="mb-5 g-3">
-                  <h5>{subfunction.name}</h5>
-                  <div className="col-md-10">
-                    <label htmlFor="levelInput" class="form-label">Level Rating</label>
-                    <p>{(subfunction.level_criteria_strong)}</p>
-                    <p>{(subfunction.level_criteria_adequate)}</p>
-                    <p>{(subfunction.level_criteria_weak)}</p>
+                <form key={subfunction.id} className="mb-5 ">
+                  <h4>{subfunction.name}</h4>
+                  <div className="col-md-10 mb-3">
+                    <label htmlFor="levelInput" className="form-label"><h6>Level Rating</h6></label>
+                    <ul>
+                      <li>{(subfunction.level_criteria_strong)}</li>
+                      <li>{(subfunction.level_criteria_adequate)}</li>
+                      <li>{(subfunction.level_criteria_weak)}</li>
+                    </ul>
                     <input type="range" className="form-range" id="levelInput" min="0" max="5" step="1"/>
                   </div>
 
-                  <div className=" col-md-10">
-                    <label htmlFor="findingsInput" className="form-label">Assessment Findings</label>
-                    <input className="form-control" id="findingsInput" />
+                  <div className="col-md-10 mb-3">
+                    <label htmlFor="findingsInput" className="form-label"><h6>Assessment Findings</h6></label>
+                    <input type="text" className="form-control" id="findingsInput" />
                   </div>
 
-                  <div className=" col-md-10">
-                    <label htmlFor="impactsInput" className="form-label">Impacts</label>
-                    <input className="form-control" id="impactsInput" />
+                  <div className="col-md-10 mb-3">
+                    <label htmlFor="impactsInput" className="form-label"><h6>Impacts</h6></label>
+                    <input type="text" className="form-control" id="impactsInput" />
+                  </div>
+                  <div className="row mb-3">
+                  <div className="col-md-5 mb-3">
+                    <label htmlFor="recommendationsInput" className="form-label"><h6>Recommendations</h6></label>
+                    <input type="text" className="form-control" id="recommendationsInput"/>
                   </div>
 
-                  <div className="">
-                    <label htmlFor="recommendationsInput" className="form-label">Recommendations</label>
-                    <input className="form-control" id="recommendationsInput"/>
+                  
+                  <div className="col mb-3">
+                    <label htmlFor="phaseInput" className="form-label"><h6>Phase</h6></label>
+                    <input type="text" className="form-control" id="phaseInput"/>
                   </div>
 
-                  <div className="row">
-                  <div className="">
-                    <label htmlFor="phaseInput" className="form-label">Phase</label>
-                  </div>
-
-                  <div className="input-group row">
-                    Tags
+                  <div className="col mb-3">
+                    <h6>Tags</h6>
                     {tags.map((tag) => {
                       return (
-                        <div key={tag.id}>
-                        <input type="checkbox" className="form-check-input" id={tag.id}></input>
-                        <label htmlFor={tag.id} className="form-check-label">{tag.name}</label>
+                        <div key={tag.id} className="g-3">
+                        <input type="checkbox" className="form-check-input" data-id={tag.id}></input>
+                        <label htmlFor={tag.id} className="form-check-label"> {tag.name}</label>
 
                       </div>
                       )
@@ -69,6 +78,7 @@ function AssessmentPage ({functionsArray}) {
                     {/* CHECKBOX INPUT HERE */}
                   </div>
                   </div>
+                  <hr></hr>
                 </form>
               )
             })}
