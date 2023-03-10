2257def9f5a26c3c7a792b176fc550d48eb14654
Lauren <lauren.a.heavey@gmail.com>
2023-03-10 11:12:16 -0600

    Add buttons, some routing.

--- src/components/Assessment/AssessmentForm/AssessmentPage.jsx
+++ src/components/Assessment/AssessmentForm/AssessmentPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 import AssessmentSection from "./AssessmentSection";
 
@@ -10,15 +10,18 @@ function AssessmentPage ({functionsArray}) {
   const tags = structure.tagsReducer;
   const subfunctionsArray = structure.subfunctionsReducer;
 
-  console.log('Params: ', Number(params.function_id))
-  console.log('functionsArray: ', functionsArray.length)
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+  // console.log('Params: ', Number(params.function_id))
+  // console.log('functionsArray: ', functionsArray)
   console.log('subfunctionsArray: ', subfunctionsArray)
   // console.log('functionsArray: ', functionsArray[9].function_index)
 
   return (
     <>
     {functionsArray.map((functionObject) => {
-      console.log('Function Object: ', functionObject);
+      // console.log('Function Object: ', functionObject);
       if (Number(params.function_id) === functionObject.id) {
         return (
           <div key={functionObject.id}>
@@ -26,8 +29,6 @@ function AssessmentPage ({functionsArray}) {
               <div className="progress-bar w-75"></div>
             </div>
             <h3>{functionObject.name}</h3>
-
-            {/* <AssessmentSection subfunctionsArray={subfunctionsArray}/> */}
             {subfunctionsArray.map((subfunction) => {
               return (
                 <form key={subfunction.id} className="mb-5 ">
@@ -39,28 +40,28 @@ function AssessmentPage ({functionsArray}) {
                       <li>{(subfunction.level_criteria_adequate)}</li>
                       <li>{(subfunction.level_criteria_weak)}</li>
                     </ul>
-                    <input type="range" className="form-range" id="levelInput" min="0" max="5" step="1"/>
+                    <input type="range" className="form-range" id="levelInput" min="0" max="5" step="1" data-bs-toggle="test"/>
                   </div>
 
-                  <div className="col-md-10 mb-3">
+                  <div className="col mb-3">
                     <label htmlFor="findingsInput" className="form-label"><h6>Assessment Findings</h6></label>
-                    <input type="text" className="form-control" id="findingsInput" />
+                    <textarea type="text" className="form-control" id="findingsInput" />
                   </div>
 
-                  <div className="col-md-10 mb-3">
+                  <div className="col mb-3">
                     <label htmlFor="impactsInput" className="form-label"><h6>Impacts</h6></label>
-                    <input type="text" className="form-control" id="impactsInput" />
+                    <textarea type="text" className="form-control" id="impactsInput" />
                   </div>
                   <div className="row mb-3">
                   <div className="col-md-5 mb-3">
                     <label htmlFor="recommendationsInput" className="form-label"><h6>Recommendations</h6></label>
-                    <input type="text" className="form-control" id="recommendationsInput"/>
+                    <textarea type="text" className="form-control" id="recommendationsInput"/>
                   </div>
 
                   
-                  <div className="col mb-3">
+                  <div className="col-md-2 mb-3">
                     <label htmlFor="phaseInput" className="form-label"><h6>Phase</h6></label>
-                    <input type="text" className="form-control" id="phaseInput"/>
+                    <input type="number" className="form-control" id="phaseInput"/>
                   </div>
 
                   <div className="col mb-3">
@@ -79,6 +80,23 @@ function AssessmentPage ({functionsArray}) {
                   </div>
                   </div>
                   <hr></hr>
+                  {subfunction.subfunction_index === subfunctionsArray.length
+                  ?
+                  <>
+                  <div className="d-grid gap-2 d-md-flex justify-content-md-end mb-2">
+                    <Link to="/dashboard">
+                      <button className="btn btn-primary">
+                      Cancel
+                      </button>
+                    </Link>
+                    <button className="btn btn-primary">Continue</button>
+                  </div>
+                  <div className="d-grid g-2 d-md-flex justify-content-md-end">
+                    <button className="btn btn-link" type="submit">Save for Later</button>
+                  </div>
+                  </>
+                  : <></>
+                  }
                 </form>
               )
             })}
