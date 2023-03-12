83724989e89dd0f5829a8e26450860999ebcf05f
Katie Stairs <kstairs831@gmail.com>
2023-03-12 16:03:12 -0500

    Got the table looking close to what I want.

--- src/components/Assessment/AssessmentAnswers/AssessmentAnswers.jsx
+++ src/components/Assessment/AssessmentAnswers/AssessmentAnswers.jsx
@@ -15,61 +15,86 @@ function AssessmentAnswers() {
     }, []);
 
     const goToEditPage = () => {
-        history.push(`/assessment-edit/1`)
+        history.push(`/assessment-edit/${assessmentAnswersById.assessment_id}`)
     }
-    // ${assessmentAnswersById.assessment_id}
+
     const goToOverviewPage = () => {
         history.push(`/client-overview/${assessmentAnswersById.assessment_id}`)
     }
 
     return (
         <>
-        <h1>{assessmentAnswersById.company_name} Assessment Answers</h1>
-        <h4>Bucket Function Subfunction Level Phase Tags</h4>
-        <div className="accordion" id="accordionExample">
-    {/* <h4>Bucket Function Subfunction Level Phase Tags</h4> */}
-    {/* <div className="accordion-item">
-            <h2 className="accordion-header" id="headingOne">
-            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                Bucket Function Subfunction Level Phase Tags
-            </button>
-            </h2>
-            <button>See Overview</button>
-            <button>Edit</button>
-            <div id="collapseOne" className="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                <div className="accordion-body">
+        <h2>{assessmentAnswersById.company_name} Assessment Answers</h2>
+        <div class="container shadow min-vh-100 py-2">
+            <div class="table-responsive">
+                <table class="table accordion">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Bucket</th>
+                            <th scope="col">Function</th>
+                            <th scope="col">Subfunction</th>
+                            <th scope="col">Level</th>
+                            <th scope="col">Phase</th>
+                            <th scope="col">Tags</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr data-bs-toggle="collapse" data-bs-target="#r1">
+                            <th scope="row">1 <i class="bi bi-chevron-down"></i></th>
+                            <td>{assessmentAnswersById.bucket_name || ''}</td>
+                            <td>{assessmentAnswersById.function_name || ''}</td>
+                            <td>{assessmentAnswersById.subfunction_name || ''}</td>
+                            <td>{assessmentAnswersById.level_rating || ''}</td>
+                            <td>{assessmentAnswersById.phase || ''}</td>
+                            <td>{assessmentAnswersById.tag_name || ''}</td>
+                        </tr>
+                        <tr class="collapse accordion-collapse" id="r1" data-bs-parent=".table">
+                        <table class="table accordion">
+                            <thead>
+                                <tr>
+                                    <th scope="col"></th>
+                                    <th scope="col">Findings</th>
+                                    <th scope="col">Impact</th>
+                                    <th scope="col">Recommendation</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr data-bs-toggle="collapse" data-bs-target="#r1">
+                                    <th scope="row">1 <i class="bi bi-chevron-down"></i></th>
+                                    <td>{assessmentAnswersById.findings || ''}</td>
+                                    <td>{assessmentAnswersById.function_name || ''}</td>
+                                    <td>{assessmentAnswersById.subfunction_name || ''}</td>
+                                    <td>{assessmentAnswersById.level_rating || ''}</td>
+                                    <td>{assessmentAnswersById.phase || ''}</td>
+                                    <td>{assessmentAnswersById.tag_name || ''}</td>
+                                </tr>
+                                </tbody>
+                        </table>
+                        </tr>
+                        <tr data-bs-toggle="collapse" data-bs-target="#r2">
+                            <th scope="row">2 <i class="bi bi-chevron-down"></i></th>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                        </tr>
+                        <tr class="collapse accordion-collapse" id="r2" data-bs-parent=".table">
+                            <td colspan="5"> Item 2 detail .. This is the first item's accordion body. It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow. </td>
+                        </tr>
+                        <tr data-bs-toggle="collapse" data-bs-target="#r3">
+                            <th scope="row">3 <i class="bi bi-chevron-down"></i></th>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                        </tr>
+                        <tr class="collapse accordion-collapse" id="r3" data-bs-parent=".table">
+                            <td colspan="5"> Item 3 detail .. This is the first item's accordion body. It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow. </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
-            </div>
-        </div> */}
-        <div className="accordion-item">
-            <h2 className="accordion-header" id="headingTwo">
-            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                {assessmentAnswersById.bucket_name || ''} {assessmentAnswersById.function_name || ''} {assessmentAnswersById.subfunction_name || ''} {assessmentAnswersById.level_rating || ''} {assessmentAnswersById.tag_name || ''}
-            </button>
-            </h2>
-        <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-            <div className="accordion-body">
-                <strong>Findings:</strong> {assessmentAnswersById.findings || ''} 
-                <strong>Impact:</strong> {assessmentAnswersById.impact || ''} 
-                <strong>Recommendations:</strong>{assessmentAnswersById.recommendations || ''}
-            </div>
-            </div>
-            <button onClick={goToOverviewPage}>See Overview</button>
-            <button onClick={goToEditPage}>Edit</button>
-        </div>
-        {/*
-        <div className="accordion-item">
-            <h2 className="accordion-header" id="headingThree">
-            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Accordion Item #3
-            </button>
-            </h2>
-        <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-            <div className="accordion-body">
-                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-            </div>
-            </div> 
-        </div> */}
         </div>
         </>
     )
--- src/components/Assessment/AssessmentAnswers/AssessmentAnswersItem.jsx
+++ src/components/Assessment/AssessmentAnswers/AssessmentAnswersItem.jsx
@@ -5,51 +5,57 @@
 // function AssessmentAnswersItem({ answers }) {
 
 //     return (
-//         <div className="accordion" id="accordionExample">
-//     {/* <h4>Bucket Function Subfunction Level Phase Tags</h4> */}
-//     {/* <div className="accordion-item">
-//             <h2 className="accordion-header" id="headingOne">
-//             <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-//                 Bucket Function Subfunction Level Phase Tags
-//             </button>
-//             </h2>
-//             <button>See Overview</button>
-//             <button>Edit</button>
-//             <div id="collapseOne" className="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-//                 <div className="accordion-body">
-//             </div>
-//             </div>
-//         </div> */}
-//         <div className="accordion-item">
-//             <h2 className="accordion-header" id="headingTwo">
-//             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-//                 {answers.bucket_name || ''} {answers.function_name || ''} {answers.subfunction_name || ''} {answers.level_rating || ''} {answers.tag_name || ''}
-//             </button>
-//             </h2>
-//         <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-//             <div className="accordion-body">
-//                 <strong>Findings:</strong> {answers.findings || ''} 
-//                 <strong>Impact:</strong> {answers.impact || ''} 
-//                 <strong>Recommendations:</strong>{answers.recommendations || ''}
-//             </div>
-//             </div>
-//             <button>See Overview</button>
-//             <button>Edit</button>
-//         </div>
-//         {/*
-//         <div className="accordion-item">
-//             <h2 className="accordion-header" id="headingThree">
-//             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-//                 Accordion Item #3
-//             </button>
-//             </h2>
-//         <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-//             <div className="accordion-body">
-//                 <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-//             </div>
-//             </div> 
-//         </div> */}
-//         </div>
+//     <div class="container shadow min-vh-100 py-2">
+// <div class="table-responsive">
+//     <table class="table accordion">
+//         <thead>
+//             <tr>
+//                 <th scope="col">#</th>
+//                 <th scope="col">Bucket</th>
+//                 <th scope="col">Function</th>
+//                 <th scope="col">Subfunction</th>
+//                 <th scope="col">Level</th>
+//                 <th scope="col">Phase</th>
+//                 <th scope="col">Tags</th>
+//             </tr>
+//         </thead>
+//         <tbody>
+//             <tr data-bs-toggle="collapse" data-bs-target="#r1">
+//                 <th scope="row">1 <i class="bi bi-chevron-down"></i></th>
+//                 <td>{assessmentAnswers.bucket_name || ''}</td>
+//                 <td>{answers.function_name || ''}</td>
+//                 <td>{answers.subfunction_name || ''}</td>
+//                 <td>{answers.level_rating || ''}</td>
+//                 <td>{answers.phase || ''}</td>
+//                 <td>{answers.tag_name || ''}</td>
+//             </tr>
+//             <tr class="collapse accordion-collapse" id="r1" data-bs-parent=".table">
+//                 <td colspan="5"> Item 1 detail .. This is the first item's accordion body. It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow. </td>
+//             </tr>
+//             <tr data-bs-toggle="collapse" data-bs-target="#r2">
+//                 <th scope="row">2 <i class="bi bi-chevron-down"></i></th>
+//                 <td>Cell</td>
+//                 <td>Cell</td>
+//                 <td>Cell</td>
+//                 <td>Cell</td>
+//             </tr>
+//             <tr class="collapse accordion-collapse" id="r2" data-bs-parent=".table">
+//                 <td colspan="5"> Item 2 detail .. This is the first item's accordion body. It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow. </td>
+//             </tr>
+//             <tr data-bs-toggle="collapse" data-bs-target="#r3">
+//                 <th scope="row">3 <i class="bi bi-chevron-down"></i></th>
+//                 <td>Cell</td>
+//                 <td>Cell</td>
+//                 <td>Cell</td>
+//                 <td>Cell</td>
+//             </tr>
+//             <tr class="collapse accordion-collapse" id="r3" data-bs-parent=".table">
+//                 <td colspan="5"> Item 3 detail .. This is the first item's accordion body. It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow. </td>
+//             </tr>
+//         </tbody>
+//     </table>
+// </div>
+// </div>
 // )
 // }
 
