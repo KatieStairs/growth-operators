e7001822c4c123ba58dfad94df4009a08b8d8369
Katie Stairs <kstairs831@gmail.com>
2023-03-12 16:35:19 -0500

    Completed AssessmentAnswers to a higher visual standard. Routes still aren't working, and I need to figure out how to work with the side-navBar.

--- src/components/Assessment/AssessmentAnswers/AssessmentAnswers.jsx
+++ src/components/Assessment/AssessmentAnswers/AssessmentAnswers.jsx
@@ -23,11 +23,11 @@ function AssessmentAnswers() {
     }
 
     return (
-        <>
+        <div className="assessmentAnswers">
         <h2>{assessmentAnswersById.company_name} Assessment Answers</h2>
-        <div class="container shadow min-vh-100 py-2">
-            <div class="table-responsive">
-                <table class="table accordion">
+        <div className="container shadow min-vh-100 py-2">
+            <div className="table-responsive">
+                <table className="table accordion">
                     <thead>
                         <tr>
                             <th scope="col">#</th>
@@ -37,66 +37,85 @@ function AssessmentAnswers() {
                             <th scope="col">Level</th>
                             <th scope="col">Phase</th>
                             <th scope="col">Tags</th>
+                            <th scope="col"></th>
+                            <th scope="col"></th>
+                            <th scope="col"></th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr data-bs-toggle="collapse" data-bs-target="#r1">
-                            <th scope="row">1 <i class="bi bi-chevron-down"></i></th>
+                            <th scope="row">1 <i className="bi bi-chevron-down"></i></th>
                             <td>{assessmentAnswersById.bucket_name || ''}</td>
                             <td>{assessmentAnswersById.function_name || ''}</td>
                             <td>{assessmentAnswersById.subfunction_name || ''}</td>
                             <td>{assessmentAnswersById.level_rating || ''}</td>
                             <td>{assessmentAnswersById.phase || ''}</td>
                             <td>{assessmentAnswersById.tag_name || ''}</td>
+                            <td><button>Expand</button></td>
+                            <td><button onClick={goToOverviewPage}>See Overview</button></td>
+                            <td><button onClick={goToEditPage}>Edit</button></td>
                         </tr>
-                        <tr class="collapse accordion-collapse" id="r1" data-bs-parent=".table">
-                        <table class="table accordion">
+                        <tr className="collapse accordion-collapse" id="r1" data-bs-parent=".table">
+                        <table className="table accordion">
                             <thead>
                                 <tr>
-                                    <th scope="col"></th>
-                                    <th scope="col">Findings</th>
-                                    <th scope="col">Impact</th>
-                                    <th scope="col">Recommendation</th>
+                                    {/* <th scope="col"></th> */}
+                                    <th scope="col">Findings:</th>
+                                    {/* <th scope="col">Impact</th>
+                                    <th scope="col">Recommendation</th> */}
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr data-bs-toggle="collapse" data-bs-target="#r1">
-                                    <th scope="row">1 <i class="bi bi-chevron-down"></i></th>
+                                    {/* <th scope="row"> <i className="bi bi-chevron-down"></i></th> */}
                                     <td>{assessmentAnswersById.findings || ''}</td>
-                                    <td>{assessmentAnswersById.function_name || ''}</td>
-                                    <td>{assessmentAnswersById.subfunction_name || ''}</td>
-                                    <td>{assessmentAnswersById.level_rating || ''}</td>
-                                    <td>{assessmentAnswersById.phase || ''}</td>
-                                    <td>{assessmentAnswersById.tag_name || ''}</td>
+                                    {/* <td>{assessmentAnswersById.impact || ''}</td>
+                                    <td>{assessmentAnswersById.recommendations || ''}</td> */}
                                 </tr>
                                 </tbody>
                         </table>
-                        </tr>
-                        <tr data-bs-toggle="collapse" data-bs-target="#r2">
-                            <th scope="row">2 <i class="bi bi-chevron-down"></i></th>
-                            <td>Cell</td>
-                            <td>Cell</td>
-                            <td>Cell</td>
-                            <td>Cell</td>
-                        </tr>
-                        <tr class="collapse accordion-collapse" id="r2" data-bs-parent=".table">
-                            <td colspan="5"> Item 2 detail .. This is the first item's accordion body. It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow. </td>
-                        </tr>
-                        <tr data-bs-toggle="collapse" data-bs-target="#r3">
-                            <th scope="row">3 <i class="bi bi-chevron-down"></i></th>
-                            <td>Cell</td>
-                            <td>Cell</td>
-                            <td>Cell</td>
-                            <td>Cell</td>
-                        </tr>
-                        <tr class="collapse accordion-collapse" id="r3" data-bs-parent=".table">
-                            <td colspan="5"> Item 3 detail .. This is the first item's accordion body. It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow. </td>
+                        <table className="table accordion">
+                            <thead>
+                                <tr>
+                                    {/* <th scope="col"></th> */}
+                                    {/* <th scope="col">Findings</th> */}
+                                    <th scope="col">Impact:</th>
+                                    {/* <th scope="col">Recommendation</th> */}
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr data-bs-toggle="collapse" data-bs-target="#r1">
+                                    {/* <th scope="row"> <i className="bi bi-chevron-down"></i></th> */}
+                                    {/* <td>{assessmentAnswersById.findings || ''}</td> */}
+                                    <td>{assessmentAnswersById.impact || ''}</td>
+                                    {/* <td>{assessmentAnswersById.recommendations || ''}</td> */}
+                                </tr>
+                                </tbody>
+                        </table>
+                        <table className="table accordion">
+                            <thead>
+                                <tr>
+                                    {/* <th scope="col"></th>
+                                    <th scope="col">Findings</th>
+                                    <th scope="col">Impact</th> */}
+                                    <th scope="col">Recommendation:</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr data-bs-toggle="collapse" data-bs-target="#r1">
+                                    {/* <th scope="row"> <i className="bi bi-chevron-down"></i></th>
+                                    <td>{assessmentAnswersById.findings || ''}</td>
+                                    <td>{assessmentAnswersById.impact || ''}</td> */}
+                                    <td>{assessmentAnswersById.recommendations || ''}</td>
+                                </tr>
+                                </tbody>
+                        </table>
                         </tr>
                     </tbody>
                 </table>
             </div>
         </div>
-        </>
+        </div>
     )
 };
 
