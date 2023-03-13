34a64656254a0d3137ffbee69cf59679d6917868
Katie Stairs <kstairs831@gmail.com>
2023-03-13 11:08:20 -0500

    Added the modals to AssessmentAnswers and Edit/Review pages.

--- src/components/Assessment/AssessmentAnswers/AssessmentAnswers.jsx
+++ src/components/Assessment/AssessmentAnswers/AssessmentAnswers.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
@@ -7,6 +7,7 @@ function AssessmentAnswers() {
     const history = useHistory();
     // const assessmentAnswersList = useSelector(store => store.assessmentAnswersList);
     const assessmentAnswersById = useSelector(store => store.assessmentAnswersById);
+
     useEffect(() => {
         dispatch({
             type: 'SAGA/GET_ASSESSMENT_ANSWERS_BY_ID',
@@ -27,7 +28,7 @@ function AssessmentAnswers() {
         <h1>{assessmentAnswersById.company_name} Assessment Answers</h1>
         <div className="container shadow min-vh-100 py-2">
             <div className="table-responsive">
-                <table className="table accordion">
+                <table className="table">
                     <thead>
                         <tr>
                             <th scope="col">#</th>
@@ -43,7 +44,7 @@ function AssessmentAnswers() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr data-bs-toggle="collapse" data-bs-target="#r1">
+                        <tr>
                             <th scope="row">1 <i className="bi bi-chevron-down"></i></th>
                             <td>{assessmentAnswersById.bucket_name || ''}</td>
                             <td>{assessmentAnswersById.function_name || ''}</td>
@@ -51,66 +52,56 @@ function AssessmentAnswers() {
                             <td>{assessmentAnswersById.level_rating || ''}</td>
                             <td>{assessmentAnswersById.phase || ''}</td>
                             <td>{assessmentAnswersById.tag_name || ''}</td>
-                            <td><button>Expand</button></td>
+                            <td><button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+                                Expand
+                            </button></td>
                             <td><button onClick={goToOverviewPage}>See Overview</button></td>
                             <td><button onClick={goToEditPage}>Edit</button></td>
                         </tr>
-                        <tr className="collapse accordion-collapse" id="r1" data-bs-parent=".table">
-                        <table className="table accordion">
-                            <thead>
-                                <tr>
-                                    {/* <th scope="col"></th> */}
-                                    <th scope="col">Findings:</th>
-                                    {/* <th scope="col">Impact</th>
-                                    <th scope="col">Recommendation</th> */}
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr data-bs-toggle="collapse" data-bs-target="#r1">
-                                    {/* <th scope="row"> <i className="bi bi-chevron-down"></i></th> */}
-                                    <td>{assessmentAnswersById.findings || ''}</td>
-                                    {/* <td>{assessmentAnswersById.impact || ''}</td>
-                                    <td>{assessmentAnswersById.recommendations || ''}</td> */}
-                                </tr>
-                                </tbody>
-                        </table>
-                        <table className="table accordion">
-                            <thead>
-                                <tr>
-                                    {/* <th scope="col"></th> */}
-                                    {/* <th scope="col">Findings</th> */}
-                                    <th scope="col">Impact:</th>
-                                    {/* <th scope="col">Recommendation</th> */}
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr data-bs-toggle="collapse" data-bs-target="#r1">
-                                    {/* <th scope="row"> <i className="bi bi-chevron-down"></i></th> */}
-                                    {/* <td>{assessmentAnswersById.findings || ''}</td> */}
-                                    <td>{assessmentAnswersById.impact || ''}</td>
-                                    {/* <td>{assessmentAnswersById.recommendations || ''}</td> */}
-                                </tr>
-                                </tbody>
-                        </table>
-                        <table className="table accordion">
-                            <thead>
-                                <tr>
-                                    {/* <th scope="col"></th>
-                                    <th scope="col">Findings</th>
-                                    <th scope="col">Impact</th> */}
-                                    <th scope="col">Recommendation:</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr data-bs-toggle="collapse" data-bs-target="#r1">
-                                    {/* <th scope="row"> <i className="bi bi-chevron-down"></i></th>
-                                    <td>{assessmentAnswersById.findings || ''}</td>
-                                    <td>{assessmentAnswersById.impact || ''}</td> */}
-                                    <td>{assessmentAnswersById.recommendations || ''}</td>
-                                </tr>
-                                </tbody>
-                        </table>
-                        </tr>
+                        <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Expanded Assessment Answers</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body text-center">
+                                <div>
+                                <h5>Findings</h5>
+                                <textarea
+                                    type='text'
+                                    value={assessmentAnswersById.findings}
+                                    // onChange={(evt) => setNameInput(evt.target.value)}
+                                    readOnly 
+                                />
+                                </div>
+                                <div>
+                                <h5>Impact</h5>
+                                <textarea
+                                    type='text'
+                                    value={assessmentAnswersById.impact}
+                                    rows={12}
+                                    // onChange={(evt) => setNameInput(evt.target.value)}
+                                    readOnly
+                                />
+                                </div>
+                                <div>
+                                <h5>Recommendations</h5>
+                                <textarea
+                                    type='text'
+                                    value={assessmentAnswersById.recommendations}
+                                    // onChange={(evt) => setNameInput(evt.target.value)}
+                                    readOnly 
+                                />
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Submit</button>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
                     </tbody>
                 </table>
             </div>
--- src/components/Assessment/AssessmentEdit/AssessmentEdit.jsx
+++ src/components/Assessment/AssessmentEdit/AssessmentEdit.jsx
@@ -10,6 +10,8 @@ function AssessmentEdit() {
     const history = useHistory();
     const user = useSelector(store => store.user)
 
+    const [levelRatingInput, setLevelRatingInput] = useState(assessmentAnswersById.level_rating)
+
     useEffect(() => {
         console.log('params.id', params.id)
         dispatch({
@@ -30,6 +32,21 @@ function AssessmentEdit() {
         })
     }
 
+    const updateAssessmentAnswers = (event) => {
+        console.log('Updated answers:', levelRatingInput)
+        event.preventDefault();
+        const updatedAssessmentAnswers = {
+          id: assessmentAnswersById.assessment_id,
+          level_rating: levelRatingInput,
+          image_url: itemImageUrl,
+        }
+        dispatch({
+          type: 'UPDATE_ASSESSMENT_ANSWERS',
+          payload: updatedItem
+        })
+        history.push('/shelf');
+      };
+
     const handleSubmit = () => {
         addNewHeadline();
     }
@@ -42,17 +59,11 @@ function AssessmentEdit() {
     }
 
     return (
-        <div className="assessmentEdit">
-        <h1>{assessmentAnswersById.company_name} Assessment</h1>
-        <h2>{assessmentAnswersById.bucket_name} - Review & Submit</h2>
-        <form>
-            <div className="input-group">
-                <span className="input-group-text" style={{ fontSize: 22 }}>Headline: </span>
-                <input type="text" className="form-control" id={assessmentAnswersById.assessment_id}/>
-            </div>
+        <div className="assessmentAnswers">
+        <h1>{assessmentAnswersById.company_name} Assessment Answers</h1>
         <div className="container shadow min-vh-100 py-2">
             <div className="table-responsive">
-                <table className="table accordion">
+                <table className="table">
                     <thead>
                         <tr>
                             <th scope="col">#</th>
@@ -68,7 +79,7 @@ function AssessmentEdit() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr data-bs-toggle="collapse" data-bs-target="#r1">
+                        <tr>
                             <th scope="row">1 <i className="bi bi-chevron-down"></i></th>
                             <td>{assessmentAnswersById.bucket_name || ''}</td>
                             <td>{assessmentAnswersById.function_name || ''}</td>
@@ -76,55 +87,156 @@ function AssessmentEdit() {
                             <td>{assessmentAnswersById.level_rating || ''}</td>
                             <td>{assessmentAnswersById.phase || ''}</td>
                             <td>{assessmentAnswersById.tag_name || ''}</td>
-                            <td><button>Expand</button></td>
+                            <td><button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+                                Expand
+                            </button></td>
                             <td><button onClick={goToOverviewPage}>See Overview</button></td>
-                            <td><button>Edit</button></td>
+                            {/* <td><button onClick={goToEditPage}>Edit</button></td> */}
+                            <td><button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal1">
+                                Edit
+                            </button></td>
                         </tr>
-                        <tr className="collapse accordion-collapse" id="r1" data-bs-parent=".table">
-                        <table className="table">
-                            <thead>
-                                <tr scope="col">
-                                    <th scope="col">Findings:</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr data-bs-toggle="collapse" data-bs-target="#r1">
-                                    <td>{assessmentAnswersById.findings || ''}</td>
-                                </tr>
-                                </tbody>
-                        </table>
-                        <table className="table accordion">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Impact:</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr data-bs-toggle="collapse" data-bs-target="#r1">
-                                    <td>{assessmentAnswersById.impact || ''}</td>
-                                </tr>
-                                </tbody>
-                        </table>
-                        <table className="table accordion">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Recommendation:</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr data-bs-toggle="collapse" data-bs-target="#r1">
-                                    <td>{assessmentAnswersById.recommendations || ''}</td>
-                                </tr>
-                                </tbody>
-                        </table>
-                        </tr>
+                        <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">More Assessment Answers Data</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body text-center">
+                                <div>
+                                <h5>Level</h5>
+                                <input
+                                    type='text'
+                                    value={assessmentAnswersById.level_rating || ''}
+                                    // onChange={(evt) => setNameInput(evt.target.value)}
+                                    readOnly
+                                />
+                                </div>
+                                <div>
+                                <h5>Phase</h5>
+                                <input
+                                    type='text'
+                                    value={assessmentAnswersById.phase || ''}
+                                    // onChange={(evt) => setNameInput(evt.target.value)} 
+                                    readOnly
+                                />
+                                </div>
+                                <div>
+                                <h5>Tags</h5>
+                                <input
+                                    type='text'
+                                    value={assessmentAnswersById.tag_name || ''}
+                                    // onChange={(evt) => setNameInput(evt.target.value)}
+                                    readOnly
+                                />
+                                </div>
+                                <div>
+                                <h5>Findings</h5>
+                                <textarea
+                                    type='text'
+                                    value={assessmentAnswersById.findings}
+                                    // onChange={(evt) => setNameInput(evt.target.value)}
+                                    readOnly 
+                                />
+                                </div>
+                                <div>
+                                <h5>Impact</h5>
+                                <textarea
+                                    type='text'
+                                    value={assessmentAnswersById.impact}
+                                    rows={12}
+                                    // onChange={(evt) => setNameInput(evt.target.value)}
+                                    readOnly
+                                />
+                                </div>
+                                <div>
+                                <h5>Recommendations</h5>
+                                <textarea
+                                    type='text'
+                                    value={assessmentAnswersById.recommendations}
+                                    // onChange={(evt) => setNameInput(evt.target.value)}
+                                    readOnly 
+                                />
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Submit</button>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Assessment</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body text-center">
+                                <div>
+                                <h5>Level</h5>
+                                <input
+                                    type='text'
+                                    value={assessmentAnswersById.level_rating || ''}
+                                    onChange={(evt) => setLevelRatingInput(evt.target.value)} 
+                                />
+                                </div>
+                                <div>
+                                <h5>Phase</h5>
+                                <input
+                                    type='text'
+                                    value={assessmentAnswersById.phase || ''}
+                                    // onChange={(evt) => setNameInput(evt.target.value)} 
+                                />
+                                </div>
+                                <div>
+                                <h5>Tags</h5>
+                                <input
+                                    type='text'
+                                    value={assessmentAnswersById.tag_name || ''}
+                                    // onChange={(evt) => setNameInput(evt.target.value)}
+                                />
+                                </div>
+                                <div>
+                                <h5>Findings</h5>
+                                <textarea
+                                    type='text'
+                                    value={assessmentAnswersById.findings}
+                                    // onChange={(evt) => setNameInput(evt.target.value)} 
+                                />
+                                </div>
+                                <div>
+                                <h5>Impact</h5>
+                                <textarea
+                                    type='text'
+                                    value={assessmentAnswersById.impact}
+                                    rows={12}
+                                    // onChange={(evt) => setNameInput(evt.target.value)} 
+                                />
+                                </div>
+                                <div>
+                                <h5>Recommendations</h5>
+                                <textarea
+                                    type='text'
+                                    value={assessmentAnswersById.recommendations}
+                                    // onChange={(evt) => setNameInput(evt.target.value)} 
+                                />
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Submit</button>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+
                     </tbody>
                 </table>
             </div>
         </div>
-        <button onClick={goToDashboard}>Cancel</button>
-        <button onClick={handleSubmit}>Submit</button>
-        </form>
         </div>
     )
 }
