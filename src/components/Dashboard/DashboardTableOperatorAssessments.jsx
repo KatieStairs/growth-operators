76aa59eae3d590d165d933edd93d8bfc27156565
Adam Bergstrom <abx1138@gmail.com>
2023-03-15 10:01:29 -0500

    updated dashboard get route to get client id, updated dashboard to only show Edit in Progress clients and ability to click on client name

--- src/components/Dashboard/DashboardTableOperatorAssessments.jsx
+++ src/components/Dashboard/DashboardTableOperatorAssessments.jsx
@@ -6,32 +6,38 @@ function DashboardTableOperatorAssessments({ client }) {
     // this will take the operator to the edit page
     const handleEditClick = (client) => {
         let assessmentId = client.assessment_id;
-        console.log('edit id clicked', assessmentId);
+        // console.log('edit id clicked', assessmentId);
         history.push(`/assessment-edit/${assessmentId}`)
     }
     // this will take the operator to the Details/assessment answers page
     const handleDetailsClick = (client) => {
         let assessmentId = client.assessment_id;
-        console.log('details id clicked', assessmentId);
+        // console.log('details id clicked', assessmentId);
         history.push(`/assessment-answers/${assessmentId}`)
     }
     // this will take the operator to see the assessment
     const handleSeeClick = (client) => {
         let assessmentId = client.assessment_id;
-        console.log('see id clicked', assessmentId);
+        // console.log('see id clicked', assessmentId);
         // this one will have to be changed to see assessment
         history.push(`/assessment-answers/${assessmentId}`)
     }
-
+    const handleClientClick = (client) => {
+        let clientId = client.client_id;
+        // console.log('client id clicked', clientId);
+        history.push(`/client-overview/${clientId}`)
+    }
+    if(client.status === 'Edit in Progress'){
 return (
     <tr>
-        <td>{client.company_name}</td>
+        <td onClick={() => handleClientClick(client)}><a href="#" className="pe-auto">{client.company_name}</a></td>
         <td>{new Date(client.engagement_date).toLocaleDateString()}</td>
         <td><button type="button" className="btn btn-primary" onClick={() => handleEditClick(client)}>Edit</button></td>
         <td><button type="button" className="btn btn-primary" onClick={() => handleDetailsClick(client)}>See Details</button></td>
         <td><button type="button" className="btn btn-primary" onClick={() => handleSeeClick(client)}>See Assessment</button></td>
     </tr>
 )
+}
 }
 
 export default DashboardTableOperatorAssessments;
