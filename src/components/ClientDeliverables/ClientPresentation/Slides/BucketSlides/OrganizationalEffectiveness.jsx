fa23fec83b736a68f64e0d0285a9c695f6ad8cd5
Mike Schwartz <michael.schwartz001@gmail.com>
2023-03-20 21:02:12 -0500

    working on slideshow table layout

--- src/components/ClientDeliverables/ClientPresentation/Slides/BucketSlides/OrganizationalEffectiveness.jsx
+++ src/components/ClientDeliverables/ClientPresentation/Slides/BucketSlides/OrganizationalEffectiveness.jsx
@@ -3,12 +3,11 @@ import Reveal from 'reveal.js';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 import Chart from 'chart.js/auto';
 import { Radar } from 'react-chartjs-2';
+import SlideHeader from '../SlideHeader';
 
 function OrganizationalEffectiveness () {
   const bucketData = useSelector(store => store.presentation.bucketData.bucket1)
   const bucketTags = useSelector(store => store.presentation.bucketTags.bucket1)
-
-  console.log('Bucket 1 Tags', bucketTags)
 
   const ratings = [
     bucketData.mission_vision_values_rating,
@@ -48,6 +47,7 @@ function OrganizationalEffectiveness () {
     }
   };
 
+  const title = 'Organizational Effectiveness';
 
   return (
     <>
@@ -56,73 +56,97 @@ function OrganizationalEffectiveness () {
       </section>
 
       <section>
-        <h3>Organizational Effectiveness</h3>
-        {bucketData.headline_text}
-        <div className="container">
-        <div className="bucket-tables">
-          <h4>Quick Fixes</h4>
-          <table className="table table-hover table-striped">
-            <thead>
-              <tr>
-                <th scope="col">Subfunction</th>
-                <th scope="col">Rating</th>
-                <th scope="col">Findings</th>
-                <th scope="col">Impact</th>
-                <th scope="col">Recommendations</th>
-              </tr>
-            </thead>
-            <tbody>
-              {bucketTags.map((tag) => {
-                if (tag.tag_id == '1') {
-                  return (
-                    <tr key={tag.subfunction_id}>
-                      <th scope="row">{tag.function_name}</th>
-                      <td>{tag.level_rating}</td>
-                      <td>{tag.findings}</td>
-                      <td>{tag.impact}</td>
-                      <td>{tag.recommendations}</td>
-                    </tr>
-                  )
-                } else {return}
-              })}
-            </tbody>
-          </table>
+      <div className="container mt-3">
+      <SlideHeader title={title}/>
+      <div className="grid">
+        <div className="grid-col grid-col_8 mt-3 mb-3">
+          <div className="container shadow min-vh-auto py-2 mt-3">
 
-          <h4>Fire Drills</h4>
-          <table className="table table-hover table-striped">
-            <thead>
-              <tr>
-                <th scope="col">Subfunction</th>
-                <th scope="col">Rating</th>
-                <th scope="col">Findings</th>
-                <th scope="col">Impact</th>
-                <th scope="col">Recommendations</th>
-              </tr>
-            </thead>
-            <tbody>
-              {bucketTags.map((tag) => {
-                if (tag.tag_id == 2) {
-                  return (
-                    <tr key={tag.subfunction_id}>
-                      <th scope="row">{tag.function_name}</th>
-                      <td>{tag.level_rating}</td>
-                      <td>{tag.findings}</td>
-                      <td>{tag.impact}</td>
-                      <td>{tag.recommendations}</td>
-                    </tr>
-                  )
-                }
-              })}
-            </tbody>
-          </table>
-        </div>
-        <div style={{ position: "relative", marginTop: 25, width: "30vw"}}>
-          {<Radar data={data} options={options}  marginLeft={256} width={10} height={10}/>}
+          </div>
         </div>
         </div>
+        <div className="grid">
+        <div className="grid-col grid-col_3 mt-5 ms-2">
+
+            </div>
+            </div>
+        </div>
+        <div className="grid">
+        <div className="grid-col grid-col_8 mt-3 mb-3">
+          <div className="container shadow min-vh-auto py-2 mt-3">
+
+          </div>
+        </div>
+      </div>
+          </div>
       </section>
       </>
   )
 };
 
 export default OrganizationalEffectiveness;
+
+
+// <div style={{ position: "relative", marginTop: 25, width: "30vw"}}>
+// {<Radar data={data} options={options}  marginLeft={256} width={10} height={10}/>}
+// </div>
+
+//           <h4>Fire Drills</h4>
+//           <div className="table-responsive">
+//           <table className="table">
+//             <thead>
+//               <tr>
+//                 <th>Subfunction</th>
+//                 <th>Rating</th>
+//                 <th>Findings</th>
+//                 <th>Impact</th>
+//                 <th>Recommendations</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               {bucketTags.map((tag) => {
+//                 if (tag.tag_id == 2) {
+//                   return (
+//                     <tr key={tag.subfunction_id}>
+//                       <td>{tag.function_name}</td>
+//                       <td>{tag.level_rating}</td>
+//                       <td>{tag.findings}</td>
+//                       <td>{tag.impact}</td>
+//                       <td>{tag.recommendations}</td>
+//                     </tr>
+//                   )
+//                 }
+//               })}
+//             </tbody>
+//           </table>
+//           </div>
+
+//                     <h4>Quick Fixes</h4>
+//           <div className="table-responsive">
+//           <table className="table">
+//             <thead>
+//               <tr>
+//                 <th>Subfunction</th>
+//                 <th>Rating</th>
+//                 <th>Findings</th>
+//                 <th>Impact</th>
+//                 <th>Recommendations</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               {bucketTags.map((tag) => {
+//                 if (tag.tag_id == '1') {
+//                   return (
+//                     <tr key={tag.subfunction_id}>
+//                       <td>{tag.function_name}</td>
+//                       <td>{tag.level_rating}</td>
+//                       <td>{tag.findings}</td>
+//                       <td>{tag.impact}</td>
+//                       <td>{tag.recommendations}</td>
+//                     </tr>
+//                   )
+//                 } else {return}
+//               })}
+//             </tbody>
+//           </table>
+//           </div>
